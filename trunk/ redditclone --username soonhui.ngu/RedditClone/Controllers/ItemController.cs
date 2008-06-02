f21ed093a52ba306ca24ed494ec0d170303b04b5
soonhui.ngu <soonhui.ngu@48422cad-ab4e-0410-8ec4-9d269776cdf0>
2008-06-02 09:37:45 +0000

    ge

---
+++ trunk/ redditclone --username soonhui.ngu/RedditClone/Controllers/ItemController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedditClone.Controllers
+{
+    public class ItemController : Controller
+    {
+        public void Index()
+        {
+            // Add action logic here
+        }
+    }
+}
