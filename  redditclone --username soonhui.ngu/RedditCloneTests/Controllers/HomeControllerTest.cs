6d08190045f9226098b6d6c31b74ac2bb669c86d
soonhui.ngu <soonhui.ngu@48422cad-ab4e-0410-8ec4-9d269776cdf0>
2008-06-02 09:37:45 +0000

    ge

---
+++  redditclone --username soonhui.ngu/RedditCloneTests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedditCloneTests.Controllers
+{
+    /// <summary>
+    /// Summary description for HomeControllerTest
+    /// </summary>
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void About()
+        {
+            //
+            // TODO: Add test logic	here
+            //
+        }
+
+        [TestMethod]
+        public void Index()
+        {
+            //
+            // TODO: Add test logic	here
+            //
+        }
+    }
+}
---
+++  redditclone --username soonhui.ngu/RedditCloneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RedditCloneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Esteem Innovation Sdn. Bhd.")]
+[assembly: AssemblyProduct("RedditCloneTests")]
+[assembly: AssemblyCopyright("Copyright © Esteem Innovation Sdn. Bhd. 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("193a7959-0535-4baa-8293-a37e4bf0c04f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
