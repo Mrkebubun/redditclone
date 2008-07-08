97b0d3fae3fa9ea9e8d5134db90da60adc37d4cb
soonhui.ngu <soonhui.ngu@48422cad-ab4e-0410-8ec4-9d269776cdf0>
2008-07-08 15:28:33 +0000

    Finally! can use the membership thing offered by .net framework!

---
+++ trunk/ redditclone --username soonhui.ngu/RedditClone/Controllers/RedditMembershipProvider.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RedditClone.Models;
+using System.Web.Security;
+
+namespace RedditClone.Controllers
+{
+
+    /// <summary>
+    /// Checks the User's authentication using FormsAuthentication
+    /// and redirects to the Login Url for the application on fail
+    /// </summary>
+    public class RequiresAuthenticationAttribute : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(FilterExecutingContext filterContext)
+        {
+
+            //redirect if not authenticated
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+
+                //use the current url for the redirect
+                string redirectOnSuccess = filterContext.HttpContext.Request.Url.AbsolutePath;
+
+                //send them off to the login page
+                string redirectUrl = string.Format("?ReturnUrl={0}", redirectOnSuccess);
+                string loginUrl = FormsAuthentication.LoginUrl + redirectUrl;
+                filterContext.HttpContext.Response.Redirect(loginUrl, true);
+
+            }
+
+        }
+    }
+    public class RedditMembershipProvider : MembershipProvider
+    {
+        public override int PasswordAttemptWindow
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override bool RequiresUniqueEmail
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override int MinRequiredPasswordLength
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override string PasswordStrengthRegularExpression
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool ValidateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+        public override int MaxInvalidPasswordAttempts
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public override bool RequiresQuestionAndAnswer
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override bool EnablePasswordReset
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override bool EnablePasswordRetrieval
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+
+
+            MembershipUser user = new MembershipUser(Name, username, providerUserKey,
+            email, passwordQuestion, null, isApproved, false, DateTime.Now, DateTime.Now,
+            DateTime.Now, DateTime.Now, DateTime.Now);
+
+            new UserDataLayer().AddUser(username, password);
+            status = MembershipCreateStatus.Success;
+            return user;
+
+        }
+    }
+}
