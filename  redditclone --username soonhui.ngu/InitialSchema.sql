0f6cd5047ea883053f7ea3c983eff4e9626c9674
soonhui.ngu <soonhui.ngu@48422cad-ab4e-0410-8ec4-9d269776cdf0>
2008-09-24 02:21:18 +0000

    Update the sql scripts

---  redditclone --username soonhui.ngu/InitialSchema.sql
+++  redditclone --username soonhui.ngu/InitialSchema.sql
@@ -1,22 +1,6 @@
 
 USE RedditClone
-if not exists(select * from master.dbo.sysdatabases where name ='RedditClone')
---	DROP DATABASE RedditClone
-begin
-    CREATE DATABASE RedditClone ON  PRIMARY 
-    ( NAME = N'RedditClone_Data', 
-FILENAME = N'D:\C#\Test\RedditClone\RedditClone\App_Data\RedditClone.mdf' , 
-SIZE = 2MB, 
-MAXSIZE = 20MB, 
-FILEGROWTH = 1MB )
 
-LOG ON 
-    ( NAME = N'RedditClone_Log', 
-FILENAME = N'D:\C#\Test\RedditClone\RedditClone\App_Data\RedditClone.ldf' , 
-SIZE = 1MB ,
- MAXSIZE = 10MB, 
-FILEGROWTH = 1MB )
-end
 
 if exists (select name from sys.tables 
 	where name = 'VoteHistory')
